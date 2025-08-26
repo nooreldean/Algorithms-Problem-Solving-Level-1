9b1d4b46655e9254ab8a6fdd7ed2f7dd8a963274
nooreldean <nooreldean450@gmail.com>
2025-08-26 04:30:18 +0300

    Initial project files

--- code/30.cpp
+++ code/30.cpp
@@ -2,7 +2,30 @@
 #include <string>
 #include <cmath>
 using namespace std;
+int readpositivenumber(string massage)
+{
+    int num;
+    do
+    {
+        cout << massage << endl;
+        cin >> num;
+    } while (num < 0);
+    return num;
+}
+int factorial(int num)
+{
+    cout << "***********************" << endl;
+    int f=1;
+    for (int counter=num; counter>=1; counter--)
+    {
+        f=f*counter;
+    }
+    return f;
+}
 int main()
 {
-    
+    cout << "***********************" << endl;
+    cout << factorial(readpositivenumber("please enter a positive number: ")) << endl;
+    cout << "***********************" << endl;
+    return 0;
 }
